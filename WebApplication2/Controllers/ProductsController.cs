1c0044eb5a8c723beffb846251c6dcbf8b911b63
Полина <@EVE_15_C423>
2024-03-23 13:12:33 +0300

    init

---
+++ WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
